f3d6c243044ed699013eecf412adba5aee6d6dc9
Denis Timush <alex.pijjo@gmail.com>
2022-09-18 23:16:00 +0200

    first commit

---
+++ js/renderProducts.js
@@ -0,0 +1,48 @@
+const productsContainer = document.querySelector('#products-container');
+
+// Запускаем getProducts
+getProducts();
+
+// Асинхронная функция получения данных из файла products.json
+async function getProducts() {
+	// Получаем данные из products.json
+    const response = await fetch('./js/products.json');
+    // Парсим данные из JSON формата в JS
+    const productsArray = await response.json();
+    // Запускаем ф-ю рендера (отображения товаров)
+	renderProducts(productsArray);
+}
+
+function renderProducts(productsArray) {
+    productsArray.forEach(function (item) {
+        const productHTML = `<div class="col-md-6">
+						<div class="card mb-4" data-id="${item.id}">
+							<img class="product-img" src="img/pizza/${item.imgSrc}" alt="">
+							<div class="card-body text-center">
+								<h4 class="item-title">${item.title}</h4>
+
+								<div class="details-wrapper">
+
+									<!-- Счетчик -->
+									<div class="items counter-wrapper">
+										<div class="items__control" data-action="minus">-</div>
+										<div class="items__current" data-counter>1</div>
+										<div class="items__control" data-action="plus">+</div>
+									</div>
+									<!-- // Счетчик -->
+									
+									<div class="price">
+										<div class="price__currency">${item.price} грн</div>
+									</div>
+								</div>
+
+								<button data-cart type="button" class="btn btn-block btn-outline-warning">
+								В корзину
+								</button>
+
+							</div>
+						</div>
+					</div>`;
+        productsContainer.insertAdjacentHTML('beforeend', productHTML);
+    });
+}
